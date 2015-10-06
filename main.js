7002637709e9641d00e0aeea25526ad4d20ddbd7
jrg <joey@gracey.ca>
2015-10-06 15:51:00 -0700

    now outputs to files

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-var _ = require('underscore');
+#!/bin/env node
 
-calc = function(people){
+var _ = require('underscore');
+var fs = require('fs');
+var lineByLine = require('line-by-line'); // figure this should detect both CR and CRLF endings
+
+
+var genSet = function(people){
+	if(_.uniq(people).length !== people.length) throw new Error('duplicate error');
 	var givers = people;
 	var receivers = people.slice();
 	var sets = [];
@@ -13,16 +19,40 @@ calc = function(people){
 	}
 	while(givers.length > 0){
 		var g = givers[0];
-		console.log('giver: ',g);
 		var r = chose(_.without(g));
 		givers.splice(0,1);
-		console.log('remaining givers ',givers);
 		sets.push({"giver":g,"receiver":r});
 	}
 	return sets;
+};
+
+if(!module.parent){
+	var lr = new lineByLine('names.txt');
+	var names = [];
+	lr.on('error',function(err){console.log(err);})
+	lr.on('line',function(line){names.push(line);});
+	lr.on('end',function(){
+		var set = genSet(names);
+		printToFiles(set);
+	});
+	var printToFiles = function(set){
+		if(!fs.existsSync("output")){
+			console.log('making dir');
+			fs.mkdirSync("output",0766,function(err){
+				console.log(err);
+			})
+		}
+		set.forEach(function(el,i){
+			fs.writeFile("output/"+el.giver,el.receiver,function(err){
+				if(err){ 
+					console.log("can't write to files, permissions?");
+					throw new Error(err);
+				}
+			})
+		});
+		console.log('done!');
+	}
 }
 
-module.exports = calc;
-
-
+module.exports = genSet;
 
--- test.js
+++ test.js
@@ -1,25 +1,28 @@
 var assert = require('assert');
-var people = require('./main');
+var ss = require('./main');
 var _ = require('underscore');
 
 describe('gift selector',function(){
-		describe('the empty set',function(){
-				it('should cover the empty set case ([])',function(){
-						var set = calc([]);
-						assert.equal(set.length,0);
-						}
-					)
-				})
-
-
-		describe('a non empty set',function(){
-
-				it('should produce a set of givers an recievers so that everyone gives and gets exactly one gift',function(){
-						var set = calc(["Joey","Mike","Rory","Alex","Siebren","John","Siobhan"]);
-						set.forEach(function(el,i){
-								assert.equal( _.where(set,{'giver':el.giver} ).length,1 )
-								assert.equal(_.where(set,{'receiver':el.receiver}).length,1)
-								})
-						})
-				})
+	describe('the empty set',function(){
+		it('should cover the empty set case ([])',function(){
+			var set = ss([]);
+			assert.equal(set.length,0);
+		}
+			)
+	})
+	describe('a non empty set',function(){
+		it('should produce a set of givers an recievers so that everyone gives and gets exactly one gift',function(){
+			var set = ss(["Joey","Mike","Rory","Alex","Siebren","John","Siobhan"]);
+			set.forEach(function(el,i){
+				assert.equal( _.where(set,{'giver':el.giver} ).length,1 )
+				assert.equal(_.where(set,{'receiver':el.receiver}).length,1)
+			})
 		})
+	});
+	describe('duplicate names',function(){
+		it('should throw an error',function(){
+			var names = ['joey','john','joey'];
+			assert.throws(function(){ss(names)},Error);
+		})
+	})
+})
